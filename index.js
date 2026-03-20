d85052deb2cdf75024a6dde0e07bc3a8123accbf
Tym <hello@tym.wtf>
2026-03-20 22:27:28 +0100

    Refactor mobile identify properties handling for improved structure and clarity

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 const { DefaultDeviceProperty, DefaultWebSocketManagerOptions } = require('@discordjs/ws');
 
+const DEFAULT_IDENTIFY_PROPERTIES = Object.freeze({
+  ...DefaultWebSocketManagerOptions.identifyProperties,
+});
+
 const MOBILE_IDENTIFIERS = Object.freeze({
   ios: 'Discord iOS',
   android: 'Discord Android',
@@ -24,18 +28,34 @@ function resolveMobileIdentifier(platform = 'ios') {
   return mobileIdentifier;
 }
 
-function patchMobileIdentifyProperties(platform = 'ios') {
+function createMobileIdentifyProperties(platform = 'ios') {
   const mobileIdentifier = resolveMobileIdentifier(platform);
 
-  DefaultWebSocketManagerOptions.identifyProperties = {
-    ...DefaultWebSocketManagerOptions.identifyProperties,
+  return {
+    ...DEFAULT_IDENTIFY_PROPERTIES,
+    os: DEFAULT_IDENTIFY_PROPERTIES.os ?? '',
     browser: mobileIdentifier,
     device: mobileIdentifier,
+    $os: '',
+    $browser: mobileIdentifier,
+    $device: mobileIdentifier,
+    $referrer: '',
+    $referring_domain: '',
   };
+}
+
+function patchMobileIdentifyProperties(platform = 'ios') {
+  DefaultWebSocketManagerOptions.identifyProperties = createMobileIdentifyProperties(platform);
 
   return {
+    os: DefaultWebSocketManagerOptions.identifyProperties.os,
     browser: DefaultWebSocketManagerOptions.identifyProperties.browser,
     device: DefaultWebSocketManagerOptions.identifyProperties.device,
+    $os: DefaultWebSocketManagerOptions.identifyProperties.$os,
+    $browser: DefaultWebSocketManagerOptions.identifyProperties.$browser,
+    $device: DefaultWebSocketManagerOptions.identifyProperties.$device,
+    $referrer: DefaultWebSocketManagerOptions.identifyProperties.$referrer,
+    $referring_domain: DefaultWebSocketManagerOptions.identifyProperties.$referring_domain,
   };
 }
 
@@ -55,12 +75,13 @@ function enableMobileStatus(client, options = {}) {
 
 function disableMobileStatus() {
   DefaultWebSocketManagerOptions.identifyProperties = {
-    ...DefaultWebSocketManagerOptions.identifyProperties,
+    ...DEFAULT_IDENTIFY_PROPERTIES,
     browser: DefaultDeviceProperty,
     device: DefaultDeviceProperty,
   };
 
   return {
+    os: DefaultWebSocketManagerOptions.identifyProperties.os,
     browser: DefaultWebSocketManagerOptions.identifyProperties.browser,
     device: DefaultWebSocketManagerOptions.identifyProperties.device,
   };
